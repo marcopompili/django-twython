3254659be3007b6757fb40bb584764ac0d1c0444
Marco Pompili <marcs.pompili@gmail.com>
2014-01-25 15:51:21 +0100

    Package refactoring.

--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-      name='django-twythoner',
+      name='django-twython',
       version='0.1',
-      packages=['twythoner'],
+      packages=['django_twython'],
       include_package_data=True,
       license='MIT License',
       description='Twitter integration for django using the Twython API.',
