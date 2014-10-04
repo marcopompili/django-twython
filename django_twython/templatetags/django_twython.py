8176765979246defdbc68d355aeef80f14972be4
Marco Pompili <marcs.pompili@gmail.com>
2014-10-04 17:35:57 +0200

    Added html_for_tweet template filter.

--- django_twython/templatetags/django_twython.py
+++ django_twython/templatetags/django_twython.py
@@ -4,10 +4,12 @@ Created on 14/giu/2013
 @author: Marco Pompili
 """
 
-from twython import Twython
 from django.conf import settings
-from twython import exceptions
+from django.utils.safestring import mark_safe
 from django import template
+
+from twython import Twython
+from twython import exceptions
 
 register = template.Library()
 
@@ -16,6 +18,7 @@ class TwitterTimelineNode(template.Node):
     def __init__(self, args):
         self.username = args[1]
         self.count = args[2]
+
         self.twitter = Twython(
             settings.TWITTER_CONSUMER_KEY,
             settings.TWITTER_CONSUMER_SECRET,
@@ -60,7 +63,22 @@ def twitter_user_timeline(user, *args, **kwargs):
 
         timeline = api.get_user_timeline(screen_name=user, count=kwargs.get('count', '5'))
 
-        return {'timeline': timeline}
+        return {
+            'timeline': timeline
+        }
 
     except exceptions.TwythonError as e:
         print("Django Twython Error: %s" % e)
+
+
+@register.filter(name='html_for_tweet')
+def twython_html_for_twit(value):
+    """
+        Transforms the text of the twit into an html version.
+
+        Usage::
+            {{ twit|html_for_tweet }}
+    :param value: The twit
+    :return:
+    """
+    return mark_safe(Twython.html_for_tweet(value));
