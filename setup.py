bb07a765df60fc356a2fcd021d9b88dcf6466347
Marco Pompili <marcs.pompili@gmail.com>
2014-10-14 17:28:15 +0200

    Fix to setup.py error for missing templatetags.

--- setup.py
+++ setup.py
@@ -12,16 +12,16 @@ README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-      name='django-twython',
+      name=str('django-twython'),
       version='0.1',
-      packages=['django_twython'],
+      packages=['django_twython', 'django_twython.templatetags'],
       include_package_data=True,
-      license='MIT License',
-      description='Twitter integration for django using the Twython API.',
+      license=str('MIT License'),
+      description=str('Twitter integration for django using the Twython API.'),
       long_description=README,
-      url='https://github.com/marcopompili/django-twython',
-      author='Marco Pompili',
-      author_email='marcs@emarcs.org',
+      url=str('https://github.com/marcopompili/django-twython'),
+      author=str('Marco Pompili'),
+      author_email=str('django@emarcs.org'),
       install_requires=['django', 'twython'],
       classifiers=[
             'Environment :: Web Environment',
